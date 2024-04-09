c76218ba4299e128376f25a1406b6d236f29be48
toczekmj <toczekmj@gmail.com>
2024-04-09 20:02:59 +0200

    ref

--- main.py
+++ main.py
@@ -12,18 +12,13 @@ genres = [
 
 if __name__ == '__main__':
 
-    # videos = pd.downloadPlaylist(
-    #     "https://www.youtube.com/watch?v=ZY_2E8lVvFU&list=PL-KlXQk3aUpJZocck9etS5bhfIMZc8YZx&pp=gAQBiAQB"
-    #     ,
-    #     genres[0]
-    #     ,
-    #     append=False
-    # )
-
-    videos = [
-        'pop/0.mp3',
-        'pop/1.mp3'
-    ]
+    videos = pd.downloadPlaylist(
+        "https://www.youtube.com/watch?v=ZY_2E8lVvFU&list=PL-KlXQk3aUpJZocck9etS5bhfIMZc8YZx&pp=gAQBiAQB"
+        ,
+        genres[0]
+        ,
+        append=False
+    )
 
     for file in videos:
         sa.analyzeSample(file)
