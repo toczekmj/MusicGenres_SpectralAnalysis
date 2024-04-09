93f638ef5cdf59642de2d5576e56a5889e8305bd
toczekmj <toczekmj@gmail.com>
2024-04-09 23:55:00 +0200

    added fourier visualisation function based on scipy

--- Libraries/SoundAnalyzer.py
+++ Libraries/SoundAnalyzer.py
@@ -1,9 +1,29 @@
 import os.path
-
-import pylab
+import numpy as np
+import scipy as sp
 import soundfile
 from matplotlib import pyplot as plt
-import numpy as np
+
+
+def np_plot_magnitude_spectrum(signal, title, sample_rate, frequency_ratio=1):
+    ft = sp.fft.rfft(signal)
+
+    magnitude_spectrum = np.abs(ft)
+    title = os.path.split(title)[-1]
+
+    # plot magnitude spectrum
+    fig = plt.figure(figsize=(18, 5))
+
+    frequency = np.linspace(0, sample_rate, len(magnitude_spectrum))
+    num_frequency_beans = int(len(frequency) * frequency_ratio)
+
+    fig.canvas.manager.set_window_title(title)
+
+    plt.plot(frequency[:num_frequency_beans], magnitude_spectrum[:num_frequency_beans])
+    plt.xlabel("Frequency (Hz)")
+    plt.title(title)
+    plt.show()
+    return fig
 
 
 def plot_magnitude_spectrum(signal, title, sample_rate, frequency_ratio=1):
@@ -21,7 +41,7 @@ def plot_magnitude_spectrum(signal, title, sample_rate, frequency_ratio=1):
     fig.canvas.manager.set_window_title(title)
 
     plt.plot(frequency[:num_frequency_beans], magnitude_spectrum[:num_frequency_beans])
-    plt.yscale("log")
+    # plt.yscale("log")
     plt.xlabel("Frequency (Hz)")
     plt.title(title)
 
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import librosa
+import glob
 
 import Libraries.PlaylistDownloder
 from Libraries import SoundAnalyzer as sa
+from matplotlib import pyplot as plt
 
 genres = [
     "pop",
     "rock",
     "classical",
     "dance",
-    "jazz"
+    "jazz",
+    "country"
 ]
 
 if __name__ == '__main__':
-    # path = Libraries.PlaylistDownloder.downloadMp3("https://www.youtube.com/watch?v=Rgrm-CCgDxc", "notes_no_bitrate")
-    path = Libraries.PlaylistDownloder.downloadMp3("https://www.youtube.com/watch?v=wfF0zHeU3Zs", "classical")
-    music, sr = librosa.load(path)
-    sa.plot_magnitude_spectrum(music, path, sr)
+    # path = Libraries.PlaylistDownloder.downloadMp3("https://www.youtube.com/watch?v=Rgrm-CCgDxc", f'{genres[5]}')
+    dirpath = f"/Users/toczekmj/PycharmProjects/spectralAnalysis/{genres[5]}/*"
+    files = glob.glob(dirpath)
+    charts = []
+
+    for file in files:
+        music, sr = librosa.load(file)
+        charts.append(sa.np_plot_magnitude_spectrum(music, file, sr))
+    plt.show()
