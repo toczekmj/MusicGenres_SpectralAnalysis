a6c02f3d60dea48aaa09e6d31567123d5445e398
toczekmj <toczekmj@gmail.com>
2024-04-23 10:16:41 +0200

    add graph semilog

--- Libraries/SoundAnalyzer.py
+++ Libraries/SoundAnalyzer.py
@@ -30,7 +30,7 @@ def calculate_magnitude_spectrum(signal: list, sample_rate: int = 22050):
     return normalised_magnitude_spectrum, frequency
 
 
-def plot_frequency_graph(magnitude_spectrum: list, frequency: list, beans: int,
+def plot_frequency_graph_semilog(magnitude_spectrum: list, frequency: list, beans: int,
                          size_x: int = 18, size_y: int = 5, offset_hz: int = 50):
     plt.figure(
         figsize=(size_x, size_y)
@@ -40,6 +40,26 @@ def plot_frequency_graph(magnitude_spectrum: list, frequency: list, beans: int,
     y_axis = magnitude_spectrum[int(offset_hz/2):int(beans-offset_hz/2)]
 
     plt.semilogy(
+        x_axis,
+        y_axis
+    )
+
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Magnitude ĝ(x)")
+    plt.title("Average frequency of music")
+    plt.show()
+
+
+def plot_frequency_graph(magnitude_spectrum: list, frequency: list, beans: int,
+                         size_x: int = 18, size_y: int = 5, offset_hz: int = 50):
+    plt.figure(
+        figsize=(size_x, size_y)
+    )
+
+    x_axis = frequency[int(offset_hz/2):int(beans-offset_hz/2)]
+    y_axis = magnitude_spectrum[int(offset_hz/2):int(beans-offset_hz/2)]
+
+    plt.plot(
         x_axis,
         y_axis
     )
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from Libraries import FftPreparation as fp
 from Libraries import SoundAnalyzer as sa
 
-CHUNK = 500
+CHUNK = 2000
 
 if __name__ == '__main__':
-    path = '/Users/toczekmj/PycharmProjects/spectralAnalysis/pop/One More Night.mp3'
+    path = "/Users/toczekmj/PycharmProjects/spectralAnalysis/metal/Koń na Białym Rycerzu.mp3"
 
     signal, sample_rate = sa.load_song(path)
-    x_mean, y_mean, beans = fp.prepare_matrix(signal, sample_rate, CHUNK=CHUNK, offset_hz=0)
-    sa.plot_frequency_graph(y_mean, x_mean, beans=beans, offset_hz=0)
+    x_mean, y_mean, beans = fp.matrix_average(signal, sample_rate, CHUNK=CHUNK, offset_hz=0)
+    sa.plot_frequency_graph_semilog(y_mean, x_mean, beans=CHUNK, offset_hz=0)
+    sa.plot_frequency_graph(y_mean, x_mean, beans=CHUNK, offset_hz=0)
 
